905dfd87bb95729d3c4b0c3aeadd34b560765a63
itayh <itay.hadad@live.biu.ad.il>
2024-02-01 14:55:53 +0200

    .

--- src/userService.js
+++ src/userService.js
@@ -4,18 +4,17 @@
 export const getAllUsers = () => {
     const usersJSON = localStorage.getItem('users');
     return usersJSON ? JSON.parse(usersJSON) : [];
-  };
-  
-  // Function to check if a user with a given user name already exists
-  export const isUserRegistered = (userName, password) => {
+};
+
+// Function to check if a user with a given user name already exists
+export const isUserRegistered = (userName, password) => {
     const users = getAllUsers();
     return users.some((user) => user.userName === userName && user.password === password);
-  };
-  
-  // Function to register a new user
-  export const registerUser = (user) => {
+};
+
+// Function to register a new user
+export const registerUser = (user) => {
     const users = getAllUsers();
     users.push(user);
     localStorage.setItem('users', JSON.stringify(users));
-  };
-  
+};
