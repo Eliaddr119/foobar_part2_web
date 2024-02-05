18ac2248ed5b0a68e7d4488163b9459b2b421bcf
Evyatar Paz <evyatarpaz@gmail.com>
2024-02-05 20:07:24 +0200

    feed

--- src/App/App.js
+++ src/App/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
 import SignIn from '../SignIn/SignIn';
 import SignUp from "../SignUp/signUp";
+import FeedPage from '../PostFeed/FeedPage/FeedPage';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/SignIn" element={<SignIn/>}/>
-                <Route path="/" element={<SignUp/>}/>
+                <Route path="/" element={<SignIn/>}/>
+                <Route path="/SignUp" element={<SignUp/>}/>
+
             </Routes>
         </BrowserRouter>
     );
