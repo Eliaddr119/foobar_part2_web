6de6472ecf6a90739e4eace4b7c7b0fa69677132
Eliad Drori <eliad.drori@gmail.com>
2024-02-11 12:03:29 +0200

    asdasd

--- src/PostFeed/Comment/Comment.js
+++ src/PostFeed/Comment/Comment.js
@@ -27,48 +27,122 @@ function Comment({
   };
 
   const [canEdit, setCanEdit] = useState(editEligble);
+  const [editInput, setEditInput] = useState("");
+
+
+  const handleSubmit = (event) => {
+    console.log(editInput);
+    console.log(comment)
+    
+    event.preventDefault();
+    if (editInput === "") {
+      return;
+    }
+    
+    comment.content = editInput;
+    
+  };
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setEditInput(value);
+  };
 
   return (
-    <div className="card text-bg-light" id="commentCard">
-      {canEdit && (
-        <div class="btn-group" id="editOptions">
-          <button
-            class="btn btn-success btn-sm dropdown-toggle"
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Comments Options
-          </button>
-          <ul class="dropdown-menu text-center">
-            <li>
-              <button class="dropdown-item" onClick={deleteComment}>
-                Delete Comment
-              </button>
-            </li>
-            <li>
-              <button class="dropdown-item">Edit Comment</button>
-            </li>
-          </ul>
-        </div>
-      )}
+    <><div
+      className="modal fade"
+      id="commentEditModal"
+      tabindex="-1"
+      aria-labelledby="postModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-dialog-centered modal-lg">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5" id="postModalLabel">
+              Upload a post
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <form id="textBoxPost" onSubmit={handleSubmit}>
+              <input
+                name="postContent"
+                value={editInput}
+                onChange={handleChange}
+                placeholder="Write your post here...."
+                id="editInput"
+              ></input>
 
-      <div className="d-flex">
-        <img
-          id="commentImage"
-          className="rounded-circle"
-          alt="avatar1"
-          src={comment.user.image}
-        />
-        <div className="fs-4 ms-2">
-          {comment.user.displayName}
-          <p className="fs-5 ">{comment.commentTime}</p>
+            </form>
+          </div>
+          <div className="modal-footer">
+            <button
+              className="btn btn-outline-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button
+              className="btn btn-outline-success"
+              type="submit"
+              onClick={handleSubmit}
+              data-bs-dismiss="modal"
+            >
+              Publish
+            </button>
+          </div>
         </div>
       </div>
-
-      <span className="container-fluid fs-4">{comment.content}</span>
-      <div className="ms-2 pt-2"></div>
     </div>
+    
+    
+    <div className="card text-bg-light" id="commentCard">
+        {canEdit && (
+          <div class="btn-group" id="editOptions">
+            <button
+              class="btn btn-success btn-sm dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Comments Options
+            </button>
+            <ul class="dropdown-menu text-center">
+              <li>
+                <button class="dropdown-item" onClick={deleteComment}>
+                  Delete Comment
+                </button>
+              </li>
+              <li>
+                <button class="dropdown-item"
+                data-bs-toggle="modal"
+                data-bs-target="#commentEditModal">Edit Comment</button>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        <div className="d-flex">
+          <img
+            id="commentImage"
+            className="rounded-circle"
+            alt="avatar1"
+            src={comment.user.image} />
+          <div className="fs-4 ms-2">
+            {comment.user.displayName}
+            <p className="fs-5 ">{comment.commentTime}</p>
+          </div>
+        </div>
+
+        <span className="container-fluid fs-4">{comment.content}</span>
+        <div className="ms-2 pt-2"></div>
+      </div></>
   );
 }
 export default Comment;
