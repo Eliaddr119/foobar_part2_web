740e05e83e0a307ab9decd417c4ad642c36fdac4
Eliad Drori <eliad.drori@gmail.com>
2024-02-14 18:37:15 +0200

    Added edit comment feature

--- src/PostFeed/Comment/Comment.js
+++ src/PostFeed/Comment/Comment.js
@@ -27,49 +27,92 @@ function Comment({
   };
 
   const [canEdit, setCanEdit] = useState(editEligble);
+  const [showEdit, setShowEdit] = useState(false);
+  const [commentInput, setCommentInput] = useState(comment.content);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (commentInput === "") {
+      return;
+    }
+    comment.content = commentInput;
+    setCommentList(commentList);
+    setShowEdit(false);
+  };
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setCommentInput(value);
+  };
 
   return (
     <div className="card text-bg-light" id="commentCard">
       <div className="container">
-      {canEdit && (
-        <div class="btn-group" id="editOptions">
-          <button
-            class="btn btn-success btn-sm dropdown-toggle"
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Comments Options
-          </button>
-          <ul class="dropdown-menu text-center">
-            <li>
-              <button class="dropdown-item" onClick={deleteComment}>
-                Delete Comment
+        {canEdit && (
+          <div class="btn-group" id="editOptions">
+            <button
+              class="btn btn-success btn-sm dropdown-toggle"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Comments Options
+            </button>
+            <ul class="dropdown-menu text-center">
+              <li>
+                <button class="dropdown-item" onClick={deleteComment}>
+                  Delete Comment
+                </button>
+              </li>
+              <li>
+                <button
+                  className="dropdown-item"
+                  onClick={() => setShowEdit(true)}
+                >
+                  Edit Comment
+                </button>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        <div className="d-flex" id="commentBody">
+          <img
+            id="commentImage"
+            className="rounded-circle"
+            alt="avatar1"
+            src={comment.user.image}
+          />
+          <div className="fs-4 ms-2">
+            {comment.user.displayName}
+            <p className="fs-5 ">{comment.commentTime}</p>
+          </div>
+        </div>
+
+        {!showEdit && (
+          <span className="container-fluid fs-4">{comment.content}</span>
+        )}
+        {showEdit && (
+          <span className="container-fluid fs-4">
+            <form onSubmit={(e) => handleSubmit(e)}>
+              <input
+                name="commentContent"
+                id="commentEditInput"
+                value={commentInput}
+                onChange={handleChange}
+              ></input>
+              <button
+                className="btn btn-outline-success"
+                type="submit"
+                id="commentEditConfirm"
+              >
+                publish
               </button>
-            </li>
-            <li>
-              <button class="dropdown-item">Edit Comment</button>
-            </li>
-          </ul>
-        </div>
-      )}
+            </form>
+          </span>
+        )}
 
-      <div className="d-flex" id="commentBody">
-        <img
-          id="commentImage"
-          className="rounded-circle"
-          alt="avatar1"
-          src={comment.user.image}
-        />
-        <div className="fs-4 ms-2">
-          {comment.user.displayName}
-          <p className="fs-5 ">{comment.commentTime}</p>
-        </div>
+        <div className="ms-2 pt-2"></div>
       </div>
-
-      <span className="container-fluid fs-4">{comment.content}</span>
-      <div className="ms-2 pt-2"></div>
-    </div>
     </div>
   );
 }
