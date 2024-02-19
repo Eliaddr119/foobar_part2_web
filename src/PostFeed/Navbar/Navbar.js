111316090d8aeda48b3fdef9140f2f899e7aa84d
Eliad Drori <eliad.drori@gmail.com>
2024-02-19 11:29:56 +0200

    Added logout button

--- src/PostFeed/Navbar/Navbar.js
+++ src/PostFeed/Navbar/Navbar.js
@@ -4,6 +4,7 @@ function Navbar() {
     <nav className="navbar fixed-top bg-success ">
       <div className="container-fluid  ">
         <a className="navbar-brand "><h1 id="navHead">FooBar</h1></a>
+        
         <form className= " me-2 form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example" role="search" id="formBar">
           <input
             type="search"
--- src/PostFeed/SideMenu/SideMenu.js
+++ src/PostFeed/SideMenu/SideMenu.js
@@ -1,8 +1,14 @@
 import "./SideMenu.css";
+import { useNavigate } from "react-router-dom"; 
 
 function SideMenu() {
   const storedUserObject = sessionStorage.getItem("current_usr");
   const currentUser = JSON.parse(storedUserObject);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    sessionStorage.removeItem('current_usr');
+    navigate("/");
+  }
 
   return (
     <div className="container-fluid" id="sideMenu">
@@ -27,6 +33,9 @@ function SideMenu() {
         <li class="list-group-item d-flex align-items-start ms-3  pt-4 pb-4">
           <i class=" ps-2bi bi-shop"></i>
           <span className="ms-3 fs-3">Marketplace</span>
+        </li>
+        <li class="list-group-item d-flex align-items-start ms-3  pt-4 pb-4">
+        <button className="btn btn-danger" onClick={handleLogout}>Log Out</button>
         </li>
       </ul>
     </div>
